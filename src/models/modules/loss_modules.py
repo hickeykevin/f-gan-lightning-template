6f43186e60c6b796c37bc145b6ea770a915e17fb
hickeykevin <khickey@wpi.edu>
2021-10-20 21:31:49 +0000

    added Q and V loss modules to seperate file

---
+++ src/models/modules/loss_modules.py
@@ -0,0 +1,42 @@
+#main point
+#decaring activation and conjugate functions
+ACTIVATIONS = {
+    "KLD": lambda v: v,
+    "RKL": lambda v: -torch.exp(-v),
+    "CHI": lambda v: v,
+    "SQH": lambda v: 1-torch.exp(-v),
+    "JSD": lambda v: torch.log(torch.tensor(2.)) - torch.log(1.0+torch.exp(-v)),
+    "GAN": lambda v: -torch.log(1.0 + torch.exp(-v)), 
+}
+
+CONJUGATES = {
+    "KLD": lambda t: torch.exp(t-1),
+    "RKL": lambda t: -1 - torch.log(-t),
+    "CHI": lambda t: 0.25 * t**2 + t,
+    "SQH": lambda t: t / (1. - t),
+    "JSD": lambda t: -torch.log(2.0 - torch.exp(t)),
+    "GAN": lambda t: -torch.log(1.0 - torch.exp(t)) 
+    }
+
+class Q_loss(nn.Module):
+  def __init__(self, chosen_divergence):
+    super(Q_loss, self).__init__()
+    self.conjugates = CONJUGATES
+    self.activations = ACTIVATIONS
+    self.chosen_divergence = chosen_divergence
+
+  def forward(self, v):
+    activation_output = self.activations[self.chosen_divergence](v)
+    return torch.mean(-self.conjugates[self.chosen_divergence](activation_output))
+
+
+
+class V_loss(nn.Module):
+  def __init__(self, chosen_divergence):
+    super(V_loss, self).__init__()
+    self.activations = ACTIVATIONS
+    self.chosen_divergence = chosen_divergence
+
+  def forward(self, v):
+    return torch.mean(self.activations[self.chosen_divergence](v))
+
