0db434b15c0bc65d38c336c14487a6867ebfc3d9
hickeykevin <khickey@wpi.edu>
2021-11-15 19:17:57 +0000

    modified generalized feed forward network; still relying upon string of tuple config representation

--- src/models/modules/one_class_feedforward.py
+++ src/models/modules/one_class_feedforward.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+from itertools import zip_longest
 
 class FeedforwardNeuralNetModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, rep_dim):
@@ -18,18 +19,21 @@ class FeedforwardNeuralNetModel(nn.Module):
         return out
 
 class FF(nn.Module):
-    def __init__(self, layer_shapes):
+    def __init__(self, layer_shapes, use_batch_norm=True):
       super(FF, self).__init__()
       # Put configuration layer shapes into list 
       self.layer_shapes = list(eval(layer_shapes))
-      # Create linear layers 
+      self.use_batch_norm = use_batch_norm
+      # Create linear, activation, and optional batch_norm layers layers 
       self.linears = nn.ModuleList([nn.Linear(inp, out, bias=False) for inp, out in self.layer_shapes])
-    
+      self.batch_norm_layers = nn.ModuleList(list(nn.BatchNorm1d(out) for _, out in self.layer_shapes[:-1]))
+      self.activations = nn.ModuleList(list(nn.LeakyReLU() for _ in self.layer_shapes[:-1]))
+      # Combine all layers into a list
+      self.blocks = list(zip_longest(self.linears, self.batch_norm_layers, self.activations))
+
     def forward(self, x):
-      for i, layer in enumerate(self.linears):
-        x = self.linears[i](x)
-        # If last layer, do not pass through activation function, else do so
-        if layer != self.linears[-1]:
-          x = nn.functional.leaky_relu(x)
-      
+      for block in self.blocks:
+       for layer in block:
+         if layer != None:
+          x = layer(x) 
       return x
