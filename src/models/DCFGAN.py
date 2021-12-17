817fd89cb7d3972d67ce0e60885bdd487c28786c
hickeykevin <khickey@wpi.edu>
2021-12-17 16:11:35 -0500

    implemented dcgan archtecture, made seperate files for module and ligtingmodule

---
+++ src/models/DCFGAN.py
@@ -0,0 +1,95 @@
+from src.models.modules.dcgan import Generator, Discriminator
+from src.models.modules.loss_modules import DiscriminatorLoss, GeneratorLoss
+from pytorch_lightning import LightningModule
+import torch
+from torch.distributions.uniform import Uniform
+from torchmetrics import Accuracy
+
+
+class LitDCFGAN(LightningModule):
+  def __init__(
+      self,
+      latent_dim: int = 100,
+      lr: float = 0.0002,
+      chosen_divergence: str = "KLD",
+      batch_size=64,
+      adam_beta_one: float = 0.5,
+      num_channels: int = 3
+              ):
+              
+    super().__init__()
+    self.latent_dim = latent_dim
+    self.lr = lr
+    self.chosen_divergence = chosen_divergence
+    self.batch_size = batch_size
+    self.adam_beta_one = adam_beta_one
+    self.num_channels = num_channels
+    self.save_hyperparameters()
+
+    self.generator = Generator(self.num_channels, self.latent_dim).to(self.device)
+    self.discriminator = Discriminator(self.num_channels, self.latent_dim).to(self.device)
+    
+    self.g_criterion = GeneratorLoss(chosen_divergence = self.chosen_divergence)
+    self.d_criterion = DiscriminatorLoss(chosen_divergence = self.chosen_divergence)
+    self.d_accuracy_on_generated_instances = Accuracy(num_classes=1)
+
+  def forward(self, z):
+    return self.generator.forward(z)
+
+  def sample_z(self, n):
+    z = Uniform(-1, 1).sample([n, self.hparams.latent_dim])
+    return z
+
+  def training_step(self, batch, batch_idx, optimizer_idx):
+    imgs, _ = batch
+  
+    # Train generator
+    if optimizer_idx == 0:
+        # Create sample of noise
+        z = sample_z(self.batch_size).type_as(imgs)
+        generated_images = self.forward(z)
+
+        # Discriminator output on generated instances
+        discriminator_output_generated_imgs = self.discriminator.forward(generated_images)
+
+        # Loss calculation for Generator
+        loss_G = self.g_criterion.compute_loss(discriminator_output_generated_imgs)
+
+        self.log("train/G_loss", loss_G, on_epoch=True)
+
+        output = {"loss": loss_G}
+        return output
+
+    # Train discriminator
+    elif optimizer_idx == 1:
+
+        # Discriminator output on real instances
+        discriminator_output_real_imgs = self.discriminator.forward(imgs)
+
+        # Discriminator output on fake instances
+        z = sample_z(self.batch_size).type_as(imgs)
+        generated_images = self.forward(z)
+        discriminator_output_generated_imgs = self.discriminator.forward(generated_images)
+
+        # Loss calculation for discriminator 
+        loss_D = self.d_criterion.compute_loss(discriminator_output_real_imgs, discriminator_output_generated_imgs)
+
+        # Log Metrics
+        self.log("train/D_loss", loss_D, on_epoch=True)
+        self.d_accuracy_on_generated_instances(torch.sigmoid(discriminator_output_generated_imgs), torch.zeros((imgs.size()[0], 1), dtype=torch.int8))
+        self.log("train/D_accuracy_generated_instances", self.d_accuracy_on_generated_instances, on_epoch=True)
+
+        output = {"loss": loss_D}
+        return output
+
+      
+  def configure_optimizers(self):
+      optimizer_G = torch.optim.Adam(self.generator.parameters(), lr=self.hparams.lr, betas=(self.adam_beta_one, 0.999))
+      optimizer_D = torch.optim.Adam(self.discriminator.parameters(), lr=0.1*self.hparams.lr, betas=(self.adam_beta_one, 0.999))
+
+      return [optimizer_G, optimizer_D]
+
+
+
+
+    
---
+++ src/models/modules/dcgan.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+
+class Generator(nn.Module):
+    def __init__(self, num_channel, latent_dim):
+        super(Generator, self).__init__()
+        
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d( latent_dim, 64 * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(64 * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(64 * 8, 64 * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 4),
+            nn.ReLU(True),
+            # state size. (64*4) x 8 x 8
+            nn.ConvTranspose2d( 64 * 4, 64 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 2),
+            nn.ReLU(True),
+            # state size. (64*2) x 16 x 16
+            nn.ConvTranspose2d( 64 * 2, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            # state size. (64) x 32 x 32
+            nn.ConvTranspose2d( 64, num_channel, 4, 2, 1, bias=False),
+            # state size. (num_channel) x 64 x 64
+        )
+
+    def forward(self, x):
+        return self.main(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, num_channel, latent_dim):
+        super(Discriminator, self).__init__()
+        
+        self.main = nn.Sequential(
+            # input is (num_channel) x 64 x 64
+            nn.Conv2d(num_channel, 64, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (64) x 32 x 32
+            nn.Conv2d(64, 64 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (64*2) x 16 x 16
+            nn.Conv2d(64 * 2, 64 * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (64*4) x 8 x 8
+            nn.Conv2d(64 * 4, 64 * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (64*8) x 4 x 4
+            nn.Conv2d(64 * 8, 1, 4, 1, 0, bias=False)
+        )
+
+    def forward(self, x):
+        return self.main(x)
